82adae8407904a91fb99f2ae19a7748777ab0acb
Talal Al Ajmi <talalajmi98@gmail.com>
2024-04-24 22:51:44 +0400

    added toast for scanning qr code

--- App.js
+++ App.js
@@ -2,15 +2,15 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Form from "./screens/Form";
 import Choose from "./screens/Choose";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import SplashScreen from "./screens/SplashScreen";
 import Login from "./screens/Login";
+import Toast from "react-native-toast-message";
 
 import Scanner from "./screens/Scanner";
-
 
 const Stack = createNativeStackNavigator();
 
@@ -18,7 +18,6 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-
         <Stack.Navigator initialRouteName="SplashScreen">
           <Stack.Screen
             name="SplashScreen"
@@ -35,24 +34,24 @@ export default function App() {
             component={Login}
             options={{ headerShown: false }}
           />
-     
+
           <Stack.Screen
             name="Scanner"
             component={Scanner}
             options={{ headerShown: false }}
           />
-           <Stack.Screen
+          <Stack.Screen
             name="Choose"
             component={Choose}
             options={{ headerShown: false }}
           />
-           <Stack.Screen
+          <Stack.Screen
             name="Form"
             component={Form}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
-        
+        <Toast />
       </NavigationContainer>
     </SafeAreaProvider>
   );
--- screens/Scanner.js
+++ screens/Scanner.js
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-
-
-
-
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import Toast from "react-native-toast-message";
 
 const Scanner = ({ navigation }) => {
- const [error, setError] = useState();
-const [isLoading, setIsLoading] = useState(false)
-  
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Not yet scanned')
+  const [text, setText] = useState("Not yet scanned");
 
   const askForCameraPermission = () => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })()
-  }
+      setHasPermission(status === "granted");
+    })();
+  };
 
   useEffect(() => {
     askForCameraPermission();
@@ -28,28 +25,45 @@ const [isLoading, setIsLoading] = useState(false)
   // What happens when we scan the bar code
   const handleBarCodeScanned = async ({ type, data }) => {
     setScanned(true);
-    setText(data)
-    // call endpoint to confirms the attendee
-    console.log("here")
-    console.log(data)
+    setText(data);
     try {
-      const response = await fetch(data, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email:  "majd.alkayyal@tum.de"}),
+      setIsLoading(true);
+      Toast.show({
+        type: "info",
+        position: "bottom",
+        text1: "Checking the QR Code...",
+        text2: "Please wait...",
       });
-  
-      if (!response.ok) throw new Error('Network response was not ok.');
-  
+
+      const response = await fetch(data, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: "talalajmi98@hotmail.com" }),
+      });
+
+      if (!response.ok) throw new Error("Network response was not ok.");
+
       const result = await response.json();
-      console.log(result);
+
+      Toast.show({
+        type: result.result === 400 ? "info" : "success",
+        position: "bottom",
+        text1: "QR Code is valid",
+        text2: result.message,
+      });
     } catch (error) {
+      Toast.show({
+        type: "error",
+        position: "bottom",
+        text1: "QR Code is invalid",
+        text2: error.message,
+      });
       console.log(error.message);
     } finally {
+      setIsLoading(false);
     }
-
   };
 
   // Check permissions and return the screens
@@ -57,14 +71,19 @@ const [isLoading, setIsLoading] = useState(false)
     return (
       <View style={styles.container}>
         <Text>Requesting for camera permission</Text>
-      </View>)
+      </View>
+    );
   }
   if (hasPermission === false) {
     return (
       <View style={styles.container}>
         <Text style={{ margin: 10 }}>No access to camera</Text>
-        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
-      </View>)
+        <Button
+          title={"Allow Camera"}
+          onPress={() => askForCameraPermission()}
+        />
+      </View>
+    );
   }
 
   // Return the View
@@ -73,35 +92,42 @@ const [isLoading, setIsLoading] = useState(false)
       <View style={styles.barcodebox}>
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={{ height: 400, width: 400 }} />
+          style={{ height: 400, width: 400 }}
+        />
       </View>
       <Text style={styles.maintext}>{text}</Text>
 
-      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+      {scanned && (
+        <Button
+          title={"Scan again?"}
+          onPress={() => setScanned(false)}
+          color="tomato"
+        />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   maintext: {
     fontSize: 16,
     margin: 20,
   },
   barcodebox: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     height: 300,
     width: 300,
-    overflow: 'hidden',
+    overflow: "hidden",
     borderRadius: 30,
-    backgroundColor: 'tomato'
-  }
+    backgroundColor: "tomato",
+  },
 });
 
 export default Scanner;
